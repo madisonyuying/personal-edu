46ff93d0823bfbcf72cc1a0e67ed3c9d78931936
Madison Yu-Ying <madisonyuying@gmail.com>
2021-08-21 16:37:08 -0500

    intro to errors added

--- hello-world/errors-intro.py
+++ hello-world/errors-intro.py
@@ -1,1 +1,21 @@
+# Errors 
 
+
+# python interpreter will report errors present within code. carat character (^) typically below error message with the the type of error detected
+
+
+# ZeroDivisionError
+# reported when a division operator is performed and the denominator = zero
+numerator = 5
+denominator = 0 
+ickyResult = numerator / denominator
+
+# SyntaxError 
+ # can include: misspelled keywords, missing or too many brackets or parenthesis, incorrecy operator, missing or too many quotation marks, or other unmet/ unideal condtions.
+syntaxError = 10 + 13 = 23
+
+# NameError 
+# reported when an unknown variable is detected
+numberDogs = 5
+numberDgs = numberDogs + 1 
+# would yield a name error
