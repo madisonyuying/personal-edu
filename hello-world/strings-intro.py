fe0143e6d2c2d411019dd4c2691a11b30c04d66e
Madison Yu-Ying <madisonyuying@gmail.com>
2021-08-21 16:24:36 -0500

    basic string infor

--- hello-world/strings-intro.py
+++ hello-world/strings-intro.py
@@ -1,1 +1,16 @@
+# Strings are a sequence of characters; letters, numbers, whitespace, or punctuation; strings are enclosed by double quotes ("") or single quotes ('')
+ 
+ 
+myString = "My name is Madison"
 
+# if a string needs to be broken up into multiple lines use backslash (\)
+myStringLineBreak = "My name is \ Madison"
+
+
+# String Concatenation 
+one = "Hello"
+two = "Madison"
+
+both = one + two
+
+# concatenation is the joining of two strings using the (+) addition operator
