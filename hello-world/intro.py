4cb0a43aa619c595a800dd6de9a0808644e4aa16
Madison Yu-Ying <madisonyuying@gmail.com>
2021-08-21 16:13:46 -0500

    python intro

--- hello-world/intro.py
+++ hello-world/intro.py
@@ -1,1 +1,30 @@
+#Variables: can contain letters, numbers, and underscores
+user_Name = "five_Head"
+ # to assign a value to a variable use a single (=)
 
+#python uses camel case
+
+# use hashes to create comments. comment continues until end of line
+result = 5 + 12
+    # arithmetic addition (+)
+result = 5 - 12 
+    # arithmetic subtraction (-)
+result = 5 * 12
+    # arithmetic multiplication (*)
+result = 5 / 12
+    # arithmetic division (/)
+result = 5 % 12
+    # arithmetic modulus; returns the remainder (%)
+result = 5 ** 12 
+    #arithmetic exponentatiation (**)
+
+# Plus-Equal Operator 
+add = 0 
+add += 15 
+# (+=) adds to an existing value and then assigns the new value back to the same variable
+
+#Modulo Operator (%)
+zero = 25 % 5
+# a zero would be returned becauase 25 is evenly divisible by five with no remainder
+one = 29 % 4 
+ # when 29 is divided by four there is a remainder of one thus modulo returning a one 
