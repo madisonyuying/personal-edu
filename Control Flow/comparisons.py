d69c21d783bec275706b121b6e11983966ae859f
Madison Yu-Ying <madisonyuying@gmail.com>
2021-08-22 15:31:08 -0500

    basic comparison definitions added

--- Control Flow/comparisons.py
+++ Control Flow/comparisons.py
@@ -1,1 +1,50 @@
+# Equal operator (==): it comapres two values, variables, or expressions to determine if they are the same
+ # if the values being compared are the same then the operator returns true 
+ # if the values are not the same then false is returned
 
+if "Yes" == "Yes":
+    print("They are equal")
+
+
+# Not equals operator (!=): it compares  two values, variables, or expressions to determine if they are not the same
+    # if the values being compared are the same then false is returned
+    # if the  values being compared are not the same then true is returned
+
+if "Yes" != "No":
+    print("They are not equal")
+
+# Not operator 
+    # returns the opposite of what the original expression is 
+not True 
+    # evaluates to false 
+
+# relational operators 
+    # < less than
+    # > greater than
+    # <= less than or equal to 
+    # >= greater than or equal to 
+
+if 5 > 3 :
+    print( "5 is greater than three")
+
+# Boolean Values 
+    # a data type containing values: true and false 
+    # these two values are type: bool
+
+# And operator 
+# variable1 and variable2 
+    # if both variable1 and variable2 evaluate to true then the and operator returns true 
+True and True 
+    # returns true 
+True and False 
+    # returns false
+
+# Or operator 
+# variable1 or variable2
+    # if one of  the two variables being compared is true then true will return 
+True or True 
+    # returns true 
+True or False 
+    # returns true 
+False or False
+    # returns false 
