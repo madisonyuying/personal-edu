510bf36a299409ba82dff2162981bd096d7f57e5
Madison Yu-Ying <madisonyuying@gmail.com>
2021-08-22 15:39:55 -0500

    if, elif, else statements added

--- Control Flow/if-intro.py
+++ Control Flow/if-intro.py
@@ -1,1 +1,10 @@
+# If statements 
+average = 57
 
+if average > 50 : # an if statement is followed by a colon
+    # if the if statement is evaluated to true then the code block following it is executed. if the if statement evaluates to false then the code block is skipped and the next line of code with the same level of indention is executed 
+    print("The average is greater than fifty")
+elif average < 50 : # an elif statement continues the check and is performed after the initial if statement 
+    print("The average is less than fifty")
+else : 
+    print("The average is fifty")
