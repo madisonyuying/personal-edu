64bbcc1ec754f6961f001662a5dd9547950e9e87
Madison Yu-Ying <madisonyuying@gmail.com>
2021-08-23 15:07:07 -0500

    elementary list functions added

--- lists/lists.py
+++ lists/lists.py
@@ -21,3 +21,22 @@ allDays[0] # monday
 allDays[-1]     #selects last element: sunday
 allDays[-2:]    # selects the last two elements in the list: saturday sunday
 allDays[:-1]    # selects everything but the last element 
+
+# Determining list Length len() function 
+    # function used to determine the number of items found in the list 
+prime = [1, 2, 3, 5, 7, 11]
+length = len(prime)
+
+
+# Slicing 
+    # slice/ sub-list of list element can be selected using a colon that separates a start point and end point
+    # myList[START:END] slice includes start number and everything until end number. Excludes end number
+pencilPouch = ['pens', 'pencil', 'eraser', 'highlighter']
+pencilPouchSlice = pencilPouch[0:2]
+
+# sorted() function
+    # accepts a list as an argument and returns a new sorted list 
+unsorted = [2, 7, 4, 3, 6]
+sortedList = sorted(unsorted)
+print(sortedList)
+    # would print 2 3 4 6 7 
