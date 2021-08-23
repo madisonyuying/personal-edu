3e60e8ce9c721ed78e7e1b823b2d41c0aa755763
Madison Yu-Ying <madisonyuying@gmail.com>
2021-08-23 14:34:46 -0500

    intro to modify 2D lists

--- lists/2D-list-intro.py
+++ lists/2D-list-intro.py
@@ -1,1 +1,11 @@
+# modifying 2D lists
+    # [elemenInSublist] = newValue
+nameAnimals = [["Amanda", "Dog"], ["Bianca", "Cat"], ["Cat", "Lizard"]]
+nameAnimals[0][1] = "Horses"
+print(nameAnimals)
+    # [["Amanda", "Horses"], ["Bianca", "Cat"],["Cat", "Lizard"]]
 
+# accessing 2D lists
+    # list[sublist_index][element_in_sublist_index]
+age = [["Jake", 45], ["Aiden", 25], ["Parker", 32]]
+jakeAge = age[0][1]
