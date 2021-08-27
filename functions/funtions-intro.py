81cd93ffc1256ae726de444b54e743e78a25475c
Madison Yu-Ying <madisonyuying@gmail.com>
2021-08-26 19:00:21 -0500

    function intro added

--- functions/funtions-intro.py
+++ functions/funtions-intro.py
@@ -1,1 +1,22 @@
+# functions 
+    # defined by def keyword 
+def function1(input):
+    five = 5 
+    return input + five
+    #indentation identifies blocks of code. code in same block indented at same level
 
+# to call a function 
+# functionName()
+function1(7)
+
+# parameters: variables defined in the function definition
+# cannot be accessed outside of function
+    # five in the above example is a parameter
+
+# input is an argument
+# multiple arguments can be input; separate them by commas
+# keyword arguments can be given default values if no new values are passed to them
+
+# return prints the following variable, string, etc
+
+# local variables behave identically to function parameters. cannot be referenced outside of function either
