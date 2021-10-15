ea173b894603ff05d8ecdff9f620133c5b644cff
madison <madisonyuying@gmail.com>
2021-10-15 08:13:18 -0500

    chapter two sections one and two added

--- Chapter2/variables-dataTypes.cpp
+++ Chapter2/variables-dataTypes.cpp
@@ -1,1 +1,30 @@
+#include <stdio.h> /* preprocessor directive must be included */
+/* stdio.h is a library that contains printf and scanf definitions */
 
+/* int is a reserved word that initializes functions */
+int main(void) /* defining the function type with main*/
+
+/* if function output defined as double; stores real numbers. if void; main function receives no data from os; if return; returns control from main to the operating system*/
+{ 
+  int variable_list = 6; /* c ends statements with semi-colons*/  
+  /* int contains values -2147483647 to 2147483647 */
+
+  double rate = 5;
+  /* double type variables can be any real number*/
+  
+  short short_variable = 87;
+  /* short variables range -32767 to 32767 */
+
+  unsigned short unsigned_short = 53000;
+  /* unsigned short range from zero to 65535 */
+ 
+  unsigned unsigned_num = 5900;
+  /* unsigned range from zero to 4294967295 */
+
+  char character = 'f';
+  /* char uses single quotes*/
+  
+
+
+
+}
