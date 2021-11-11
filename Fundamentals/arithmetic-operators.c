d6c1c4614085968d747a04d62757452e3b74b794
Madison Yu-Ying <madisonyuying@gmail.com>
2021-11-10 19:25:50 -0600

    init commit for arithmetic

--- Fundamentals/arithmetic-operators.c
+++ Fundamentals/arithmetic-operators.c
@@ -1,1 +1,18 @@
+void arithmetic_operators(void);
+/* Name: void arithmetic_operators(void)
+* Desc: Demonstration of Arithmetic Operators
+*/
 
+void arithmetic_operators(void)
+{
+    int a_int;
+    int b_int;
+    double a_double;
+    double b_double;
+
+    a_int = 10;
+    b_int = 3;
+    a_double = 10.0;
+    b_double = 3.0;
+
+}
