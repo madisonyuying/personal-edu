0695c215f2b94b823ca09b74ca7265b2ae3886dc
Madison Yu-Ying <madisonyuying@gmail.com>
2021-12-07 15:40:46 -0600

    all fundamentals update

--- Fundamentals/all-fundamentals.c
+++ Fundamentals/all-fundamentals.c
@@ -1,1 +1,35 @@
+/*
+ * File: main_template.c
+ * Copy: Copyright (c) 2021 Gregory C. Myers
+ * BlazerID: mmcsw7
+ * Desc: Driver for testing concepts
+ */
+ 
+ /*
+  * Name: int main(int argc, char** argv)
+  * Desc: Used to drive other functions for the purpose to demo features in class. Function calls are commented/ uncommmented dsepending on which features are to  be tested
+  */
+  
+  /* 
+   * Name: <name of function>
+   * Desc: <description of function>
+   * Args: <list of input and output args, type, etc.>
+   */
+
+  /* 
+   * File: P1.c
+   * Author: mmcsw7
+   * 
+   * created Dec 7, 2021
+   */
+   
+#include <stdio.h>
+#include <stdio.h>
+
+
+}
+
+
+
+
 
