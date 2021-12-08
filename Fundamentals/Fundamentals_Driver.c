d745cc119bdb7a5933cad1383a83c016efd934af
Madison Yu-Ying <madisonyuying@gmail.com>
2021-12-08 10:24:20 -0600

    for loops two

--- Fundamentals/Fundamentals_Driver.c
+++ Fundamentals/Fundamentals_Driver.c
@@ -25,6 +25,8 @@
    
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
 
 #define EXIT_SUCCESS 0
@@ -44,11 +46,116 @@ void formatting_integers(void);
 void formatting_doubles(void); 
 double approximate_pi(void); 
 void type_casts(void); 
+void interactive_01(void);
+void parametric_01(const char first_name[], const char last_name[], const char 
+course_department[], const int course_number );
+void for_loops_01(void);
+void for_loops_02(void);
 
+
+
+void help(void);
 
 
 int main (int argc, char** argv){
    int index;
+   char first_name[10];
+   char last_name[10];
+   char course_department[3];
+   int course_number;
+   // argc - count of the number of argumentse
+   // argv - array of arguments from the command line
+   // indexing is zero based 
+   printf("argv: %d\n", argc);
+   for (index = 0; index < argc; index++ ) {
+      printf("argv[ %d ] has the value %s. \n", index, argv[ index ]);
+   }
+
+
+   // how to test what arguments are coming from the command line 
+   // must have at least one argument --- > the name of the executable 
+   // use an if statement to determine which function to call 
+   // use the function name as the command line argument (arbitrary)
+/*
+    if ( argc < 2 ) {
+      help(); // implicit call for help
+   } else if (( strcmpi ( argv[ 1 ], "/h" ) == 0) || (strcmpi( argv[ 1 ], "/?") == 0 )) {
+      help(); // explicit call for help
+   } else if (strcmpi( argv[ 1 ], "/get_info") == 0) {
+      get_info(); // calls our function ....
+   } else if( strcmpi( argv[ 1 ], "/Figure_2_1") == 0) {
+      Figure_2_1(); // calls our demo Figure_2_1
+   } else if( strcmpi( argv[ 1 ], "/arithmetic_operators") == 0) {
+      arithmetic_operators(); // call to arithmetic operators demo 
+   } else if( strcmpi( argv[ 1 ], "/integer_division") == 0) {
+      integer_division(); // call to integer divisioin demo 
+   } else if( strcmpi( argv[ 1 ], "/integer_remainder") == 0) {
+      integer_remainder(); // call to integer_remainder demo 
+   } else if( strcmpi( argv[ 1 ], "/formatting_integers") == 0) {
+      formatting_integers(); // call to format integers demo
+   } else if( strcmpi( argv[ 1 ], "/formatting_doubles") == 0) {
+      formatting_doubles(); // call to format doubles demo 
+   } else if( strcmpi( argv[ 1 ], "type_casts") == 0) {
+      type_casts(); // call to type casts demo 
+   } else if( strcmpi( argv[ 1 ], "/interactive_01") == 0) {
+      interactive_01(); // call to first interactive demo 
+   } else if( strcmpi( argv[ 1 ], "/parametric_01") == 0) {
+   
+      
+      //capture the information from the user... 
+
+      printf("Please enter a first name:\n");
+      scanf("%s", &first_name);
+
+      printf("Please enter a last name:\n");
+      scanf("%s", &last_name);
+
+      printf("Please enter a course department:\n");
+      scanf("%s", &course_department);
+
+      printf("Please enter a course number: \n ");
+      scanf("%d", &course_number);
+
+      // call to our output function
+      parametric_01( first_name, last_name, course_department, course_number);
+      //capture the information from the user... 
+
+      printf("Please enter a first name:\n");
+      scanf("%s", &first_name);
+
+      printf("Please enter a last name:\n");
+      scanf("%s", &last_name);
+
+      printf("Please enter a course department:\n");
+      scanf("%s", &course_department);
+
+      printf("Please enter a course number: \n ");
+      scanf("%d", &course_number);
+
+      // call to our output function
+      parametric_01( first_name, last_name, course_department, course_number );
+   } else if (( strcmpi(argv[ 1 ], "/parametric_01") == 0) && (argc == 6)) {
+      strcpy( first_name, argv[2]);
+      strcpy( last_name, argv[3]);
+      strcpy( course_department, argv[4]);
+      course_number = atoi( argv[5]);
+
+      parametric_01(first_name, last_name, course_department, course_number);
+   } else if (strcmpi (argv[1], "/for_loops_01" ) == 0) {
+      for_loops_01();
+   } else if (strcmpi (argv[1], "/for_loops_02" ) == 0) {
+      for_loops_02();
+
+   } else {
+      help(); // implicit call for help 
+   }
+   */
+   
+
+
+
+
+   
    // get_info(); // calls our get_info function
    // Figure_2_1(); // calls figure_2_1
    // projectile_1D_v1(); // calls 1D version
@@ -59,9 +166,55 @@ int main (int argc, char** argv){
    // integer_remainder(); // calls integer_remainder demo 
    // formatting_integers(); // calls  formatting_integers
    // formatting_doubles(); 
-   type_casts(); // calls type_casts
+   // type_casts(); // calls type_casts
+   //interactive_01(); // calls interactive_01
+   
+   //capture the information from the user... 
+
+   /* printf("Please enter a first name:\n");
+      scanf("%s", &first_name[10]);
+
+      printf("Please enter a last name:\n");
+      scanf("%s", &last_name[10]);
+
+      printf("Please enter a course department:\n");
+      scanf("%s", &course_department[3]);
+
+      printf("Please enter a course number: \n ");
+      scanf("%d", &course_number);
+
+      // call to our output function
+      parametric_01( first_name, last_name, course_department, course_number);
+   */
+   // for_loops_01(); // calls our for_loops_01 demo 
+   for_loops_02(); // calls our for_loops_02 demo 
+
+
     return (EXIT_SUCCESS);
+
 }
+void help(void) {
+   printf("Usage:\n");
+   printf(" fundamentals_demo.exe /h                           ... calls help \n");
+   printf(" fundamentals_demo.exe /get_info                    ... calls get_info \n");
+   printf(" fundamentals_demo.exe /Figure_2_1                  ... calls Figure_2_1\n");
+   printf(" fundamentals_demo.exe /arithmetic_operators        ... calls arithmetic_operators\n");
+   printf(" fundamentals_demo.exe /integer_division            ... calls integer_divisioin\n");
+   printf(" fundamentals_demo.exe /integer_remainder           ... calls integer_remainder\n"); 
+   printf(" fundamentals_demo.exe /formatting_integers         ... calls formatting_integers\n");
+   printf(" fundamentals_demo.exe /formatting_doubles          ... calls formatting_doubles\n");
+   printf(" fundamentals_demo.exe /type_casts                  ... calls type_casts\n");
+   printf(" fundamentals_demo.exe /interactive_01              ... calls interactive_01\n");
+   printf(" fundamentals_demo.exe /parametric_01               ... calls parametric_01 \n");
+   printf(" fundamentals_demo.exe / parametric_01 Madison YuYing EE 233          ...calls parametric_01 cmd line args \n");
+   printf(" fundamentals_demo.exe /for_loop_01                 ... calls for_loop_01");
+   printf(" fundamentals_demo.exe /for_loop_02                 ... calls for_loop_02");
+
+
+   
+}
+
+
 /* Name: get_info
 * Desc: prints information about author
 * arg: returns integer for success/failure
@@ -374,4 +527,133 @@ void type_casts(void)
     char_a = (char) 97; // lowercase a
     
     printf("========================================\n");
+}
+
+
+/*
+ * Name: void interactive_01(void)
+ * Desc: Demonstration of scanf and printf interactive
+ */
+void interactive_01(void)
+{
+   char first_name[10];
+   char last_name[10];
+   char course_department[3];
+   int course_number;
+
+   // collect this information from the user
+   printf("Please enter your first name (no more than 9 characters):\n");
+   scanf("%s", &first_name[10]);
+
+   printf("Please enter your last name (no more than 9 characters):\n");
+   scanf("%s", &last_name[10]);
+
+   printf("Please enter a course department (no more than 2 characters):\n");
+   scanf("%s", &course_department[3]);
+
+   printf("Please enter a course number: \n");
+   scanf("%d", &course_number);
+   
+   // print the information out in a nice format 
+   printf("Hello %s %s! Welcome to %s %d!\n", first_name, last_name, course_department, course_number);
+
+}
+
+/* 
+ * Name: void for_loops_01(void)
+ * Desc: Demonstration of different ways to use for loops
+ */
+void for_loops_01(void) {
+    int index;
+    double current;
+    
+    /* Syntax:
+     *      initialization expression - where you want the loop to start
+     *      loop repetition condition - must evaluate to "true" for the loop to continue 
+     *      update expression - how you want the loop to increment/ decrement
+     *  for ( initialization expression; loop repetition condition; update 
+expression)
+     *  {   
+     *      block of code/statements
+     *      statement;
+     *  }
+     */ 
+   
+    printf("index          value\n");
+    printf("======================================\n");
+    // for ( index = 0; index <= 9; index = index + 1 ){
+       for ( index = 0 ; index <= 9 ; index++) {
+       printf("%d          %d\n", index, 2*index );
+
+    }
+    printf("======================================\n\n");
+
+    printf("index          value\n");
+    printf("======================================\n");
+    for ( index = 9; index >= 0 ; index = index -1 ){
+         printf("%d          %d\n", index, 2*index );
+    }
+    printf("======================================\n");
+
+
+    printf("current         value\n");
+    printf("======================================\n");
+      for (current = 0.0; current <= 2.0; current = current + .1 ){
+         printf("%.2f            %.2f\n", current, pow(current, 2.0)); 
+      }
+    printf("======================================\n\n");
+
+    printf("current         value\n");
+    printf("======================================\n");
+      for ( index = 0; index < 0; index = index + 1){
+         printf("%d          %d\n", index, 2*index );
+      }
+    printf("======================================\n\n");
+}
+
+
+/* 
+ * Name: void for_loops_02(void)
+ * Desc: Demonstration of different ways to use for loops
+ */
+void for_loops_02(void) {
+    int index_outer;
+    int index_inner;
+    double current_outer;
+    double current_inner;
+    
+    printf("index          value\n");
+    printf("======================================\n");
+      for ( index_outer = 1; index_outer <= 12; index_outer = index_outer + 1) {
+         for ( index_inner = 1; index_inner <= 10; index_inner = index_inner + 1 ){
+            // use the indeces to fill out our multiplication table 
+            // index inner x index outer --> total number of  times this code will run 
+
+            printf(" %d, %d      %d\n", index_outer, index_inner, (index_outer * index_inner));
+
+         }
+      }
+    printf("======================================\n");        
+
+    printf("current          value\n");
+    printf("======================================\n");
+    for (current_outer = 0.0; current_outer < 10.0; current_outer = current_outer + 1.0 ) {
+       for (current_inner = 0.0; current_inner < (2 * M_PI ); current_inner = current_inner + ( M_PI / 4.0)){
+            printf("%.2f, %.4f      %.4f\n", current_outer, current_inner, (current_outer * current_inner));
+       }
+    }
+    printf("======================================\n");   
+    
+
+    for( index_outer = 0; index_outer < 12 ; index_outer++) {
+       for ( index_inner = 0; index_inner < 10; index_inner++){
+          printf("%d,", (index_outer * index_inner));
+       }
+    }
+
+      
+    printf("index          value\n");
+    printf("======================================\n");
+    printf("======================================\n");   
+       
 }
